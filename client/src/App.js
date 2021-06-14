c5a5e36cc6fc8326376f1f669b43252c6b69b1a0
Kevin Mathews <kev.math@yahoo.com>
2021-06-14 16:54:47 -0700

    client side app running

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import logo from './assets/spacex.png';
+
+  function App() {
+    return (
+    <div className="App">
+      <img src={logo} alt='spaceX logo' style={{width: '40%', display: 'block', margin: 'auto'}} />
+    </div>
+  );
+}
+
+export default App;
