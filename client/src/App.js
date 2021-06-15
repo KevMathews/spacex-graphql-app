f1ce9e8f5fa0021fdd039044e0ed136b1bdf1e8b
Kevin Mathews <kev.math@yahoo.com>
2021-06-14 18:31:39 -0700

    working on show page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import logo from './assets/spacex.png';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Launches from './components/Launches';
+
+const client = new ApolloClient({
+  uri:'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
 
   function App() {
     return (
-    <div className="App">
-      <img src={logo} alt='spaceX logo' style={{width: '40%', display: 'block', margin: 'auto'}} />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <img src={logo} alt='spaceX logo' style={{width: '40%', display: 'block', margin: 'auto'}} />
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
