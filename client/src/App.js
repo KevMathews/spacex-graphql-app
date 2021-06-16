69be3fe004d7a9f57c3a1d7fe00b6bbff9a7fe96
Kevin Mathews <kev.math@yahoo.com>
2021-06-15 23:00:22 -0700

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './assets/spacex.png';
 import { ApolloClient, ApolloProvider, from, InMemoryCache } from '@apollo/client';
 import Launches from './components/Launches';
 import Launch from './components/Launch';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const client = new ApolloClient({
@@ -15,7 +16,9 @@ const client = new ApolloClient({
       <Router>
       <div className="App container">
         <img src={logo} alt='spaceX logo' style={{width: '40%', display: 'block', margin: 'auto'}} />
+        <Navbar />
         <Route exact path='/' component={Launches} />
+        <Route exact path='/launches' component={Launches} />
         <Route exact path='/launch/:flight_number' component={Launch} />
       </div>
       </Router>
